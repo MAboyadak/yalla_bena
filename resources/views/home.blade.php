01066bf5847081516147d9c33632e81ae1d63899
asmaasheaba <asmaa.shiba2018@gmail.com>
2023-02-15 22:14:17 +0200

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,12 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="col-md-6 text-center my-5 p-3"> 
+            <h2>Latest Orders</h2>
+        </div>
+        <div class="col-md-6 text-center my-5 p-3 ">
+            <h2>Frindes Activity</h2>
+        </div>
         </div>
     </div>
 </div>
