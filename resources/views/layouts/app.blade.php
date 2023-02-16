24ca410c5e40f6805c53c830eb98908af8a45e05
asmaasheaba <asmaa.shiba2018@gmail.com>
2023-02-16 13:48:34 +0200

    asmaa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,13 +21,12 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{-- {{ config('app.name', 'Laravel') }} --}}
-                    <h2>Yalla Notlob</h2>
+                    {{ config('app.name', 'Laravel') }}
                 </a>
 
                 <a class="nav-link mx-5" href="{{route('home')}}">Home</a>
-                <a class="nav-link mx-5" href="{{route('friends.index')}}">Friends</a>
-                <a class="nav-link mx-5" href="{{route('groups.index')}}">Groups</a>
+                <a class="nav-link" href="{{route('friends.index')}}">Friends</a>
+                <a class="nav-link" href="{{route('groups.index')}}">Groups</a>
                 <a class="nav-link" href="#">Orders</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
