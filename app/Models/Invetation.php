1c9803b8f837c8b3330cbfdc2376b7c6450a67ca
hendshaaaban123 <hendshaaban123z@gamil.com>
2023-02-19 17:30:21 +0200

    new_migrations

--- app/Models/Invetation.php
+++ app/Models/Invetation.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
+class Invetation extends Model
 {
     use HasFactory;
-    protected $fillable = ['user_id','meal',''];
 }
